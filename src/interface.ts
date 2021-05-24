a37dab5564398b42b3c7a520ae1479385ed6efe1
zhangyu1818 <723156735@qq.com>
2021-05-24 22:12:56 +0800

    add comments

--- src/interface.ts
+++ src/interface.ts
@@ -7,18 +7,37 @@ export type OutDir = {
 }
 
 export interface UserConfig {
+  /**
+   * entry path
+   */
   entry?: string
+  /**
+   * output folder name
+   */
   outDir?: OutDir
+  /**
+   * pattern used to match files, only on 'onlyDependencyFile = false'
+   */
   pattern?: string
   browserslist?: string | string[] | { [key: string]: string }
   babelPlugins?: PluginItem[]
+  /**
+   * copy original style file to outDir,default true
+   */
   copyOriginalStyle?: boolean
+  /**
+   * lib-tool default matches files using the pattern,
+   * this option will only matches dependent files.
+   */
   onlyDependencyFile?:
     | boolean
     | {
         tsConfigPath?: string
         webpackConfigPath?: string
       }
+  /**
+   * filter files
+   */
   fileFilter?: (path: string) => boolean
   lessOptions?: Less.Options
   sassOptions?: Omit<SassOptions, 'file'>
